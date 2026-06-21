c080a81713dac557fd5df68c08f7e2d7bab1dbda
hexiongwu1995_local <hexiongwu1995@outlook.com>
2026-06-21 19:42:46 +0800

    some changes

--- mathjax-config.js
+++ mathjax-config.js
@@ -4,7 +4,7 @@ window.MathJax = {
         font: 'mathjax-modern'
     },
     asciimath: {
-        delimiters: [['``', '``'], ['`', '`']]
+        delimiters: [['$$', '$$'], ['$', '$']]
     },
     startup: {
         ready() {
@@ -12,7 +12,7 @@ window.MathJax = {
             Object.assign(AsciiMath.prototype, {
                 _compile: AsciiMath.prototype.compile,
                 compile(math, document) {
-                    math.display = (math.start?.delim === '``');
+                    math.display = (math.start?.delim === '$$');
                     const result = this._compile(math, document);
                     const mstyle = result.childNodes[0].childNodes.pop();
                     mstyle.childNodes.forEach(child => result.appendChild(child));
